0d8008410bdf3fa5157ca533ef2bdab7cd50585a
HernandoSilvaLeal <hernandosilvaleal@gmail.com>
2022-03-26 23:41:50 -0500

    Avance repaso python

---
+++ ejemplo3.py
@@ -0,0 +1,15 @@
+x = 5
+
+y = "Hello, World!"
+
+z = True
+
+def cosianpirulito():
+    print("x")
+
+print(type(cosianpirulito()))
+print(type(y))
+print(type(z))
+
+
+
---
+++ holamundo.py
@@ -0,0 +1,14 @@
+def saludar():
+    print("\n Hola Soy un super programa para el Mundo \n")
+
+saludar()
+
+# Este es uno de lo programas mas geniales y este es mi comentario que NO se ejecuta
+
+def Multiplicar():
+    x = input("Ingresa numero: ")
+    y = input("Ingresa numero: ")
+    z = x * y
+    print(z)    
+
+Multiplicar()
